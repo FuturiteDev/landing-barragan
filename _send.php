66fc7a09070953a3db680f59bff490053f69466d
Josué Osorio <josue.osorio@futurite.com>
2022-02-23 11:40:53 -0600

    Ajustes para la compatibilidad de versión  del servidor

--- _send.php
+++ _send.php
@@ -4,8 +4,8 @@ try {
 	$message = "Se ha llenado la forma de contacto con la siguiente información:\n\n";
 
     //Validaciones
-	if( (preg_match("/\b(?:(?:https?|ftp|http):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $_POST["mensaje"])) ){
-		echo json_encode(['status' => false, 'message' => "Favor de no ingresar links o enlaces externos en la forma de contacto."]);
+	if( preg_match("/\b(?:(?:https?|ftp|http):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $_POST["mensaje"]) ){
+		echo json_encode(array('status' => false, 'message' => "Favor de no ingresar links o enlaces externos en la forma de contacto."));
 		exit;
 	}
 
@@ -13,17 +13,17 @@ try {
     $token = $_POST['token'];
     $handle = curl_init();
     
-    curl_setopt_array($handle, [
+    curl_setopt_array($handle, array(
         CURLOPT_URL => 'https://www.google.com/recaptcha/api/siteverify',
         CURLOPT_POST => true,
         CURLOPT_POSTFIELDS => http_build_query(
-            [ 
+            array( 
                 'secret' => '6LfqplYcAAAAAHUVdIUvzMNtMx_H2o9AS2QP8HeB',
                 'response' => $token
-            ]
+			)
         ),
         CURLOPT_RETURNTRANSFER => true,
-    ]);
+	));
     
    $data = curl_exec($handle);
    curl_close($handle);
@@ -39,7 +39,7 @@ try {
 		}
 
 		$handle = curl_init();
-		$postData = [
+		$postData = array(
 			'nombre' => $_POST['nombre'],
 			'correo' => $_POST['correo'],
 			'telefono' => $_POST['telefono'],
@@ -52,14 +52,14 @@ try {
 			'utm_term' => $_POST['utm_term'],
 			'token' => 'RnV0dXJpdGUyMDIw',
 			'client' => 72,
-		];
+		);
 		
-		curl_setopt_array($handle, [
+		curl_setopt_array($handle, array(
 			CURLOPT_URL => "https://omnia.futurite.com/api/save-lead-form",
 			CURLOPT_POST => true,
 			CURLOPT_POSTFIELDS => $postData,
 			CURLOPT_RETURNTRANSFER => true,
-		]);
+		));
 	
 		$data = curl_exec($handle);
 		curl_close($handle);
@@ -71,14 +71,14 @@ try {
 				'X-Mailer: PHP/' . phpversion();
 
 		if(empty($_POST)){
-			echo json_encode(['status' => false, 'message' => "No se ha podido enviar los datos"]);
+			echo json_encode(array('status' => false, 'message' => "No se ha podido enviar los datos"));
 		} else {
 			$emailResp = @mail($to, $title, $message, $headers);
-			echo json_encode(['status' => true, 'message' => "La forma de contacto se ha enviado con exito."]);
+			echo json_encode(array('status' => true, 'message' => "La forma de contacto se ha enviado con exito."));
 		}
    } else {
-      echo json_encode(['status' => false, 'message' => 'No se podido ha procesar tu solicitud por posible SPAM, intenta más tarde.']);
+      echo json_encode(array('status' => false, 'message' => 'No se podido ha procesar tu solicitud por posible SPAM, intenta más tarde.'));
    }
-} catch (\Exception $e) {
-	echo json_encode(['status' => false, 'message' => $e->getMessage()]);
+} catch (Exception $e) {
+	echo json_encode(array('status' => false, 'message' => $e->getMessage()));
 }
