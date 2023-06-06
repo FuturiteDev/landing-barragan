ae6d7e9a21953f364b23f2d3384bd3a78b3351b7
ErwinAlexis <montesalexis.KODEX@gmail.com>
2023-06-06 13:19:21 -0600

    Aviso de privacidad

--- src/partial/footer.php
+++ src/partial/footer.php
@@ -1,5 +1,7 @@
 <footer>
 	<p>
 		Copyright ©  Dr.EnriqueBarragán 2019. Todos los derechos reservados. Diseñado y desarrollado por <span><a href="https://www.futurite.com/" target="_blank">agencia de marketing digital</a> FUTURITÉ.</span>
+		<br>
+		<a href="EBG-Aviso-de-Privacidad-v2023.pdf" target="_blank">Aviso de Privacidad</a>
 	</p>
 </footer>
